206fcb474915030c600042b20a934a8c88c36ee5
Olivier Gourgue <ogourgue@naturalsciences.be>
2024-07-30 18:59:35 +0200

    Create empty function to compute w from (u, v) in preprocess.py.
    The function is empty and needs to be written.

---
+++ preprocess.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+#########################################################
+# Compute vertical velocity from horizontal components. #
+#########################################################
+
+
+def compute_w(u, v):
+
+    w = np.zeros(u.shape)
+
+    return w
+
