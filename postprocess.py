883063c370dafae6d2d5b6b88e8f8822525d7745
Olivier Gourgue <ogourgue@naturalsciences.be>
2024-08-13 17:49:46 +0200

    Update postprocess.compute_hprob to make it much faster

--- postprocess.py
+++ postprocess.py
@@ -15,20 +15,20 @@ def compute_hprob(LON, LAT, lon, lat):
     # Initialize probability density.
     P = np.zeros((len(LAT), len(LON)))
 
-    # Loop over horizontal grid cells.
-    for i in range(len(LON)):
-        for j in range(len(LAT)):
-            ind0 = lon > LON[i] - .5 * dLON
-            ind1 = lon < LON[i] + .5 * dLON
-            ind2 = lat > LAT[j] - .5 * dLAT
-            ind3 = lat < LAT[j] + .5 * dLAT
-            ind01 = np.logical_and(ind0, ind1)
-            ind23 = np.logical_and(ind2, ind3)
-            ind = np.logical_and(ind01, ind23)
-            P[j, i] = np.mean(ind)
+    # Compute grid indices for each particle positions.
+    idlon = np.round((lon - LON[0]) / dLON).astype(int)
+    idlat = np.round((lat - LAT[0]) / dLAT).astype(int)
+    idlon = np.clip(idlon, 0, len(LON) -1)
+    idlat = np.clip(idlat, 0, len(LAT) -1)
+
+    # Compute number of particles per grid cell.
+    for i in range(len(lon)):
+        P[idlat[i], idlon[i]] += 1
+
+    # Divide by total number of particles to obtain probability.
+    P /= len(lon)
 
     # Divide by cell surface area to obtain probability density.
-    for i in range(len(LAT)):
-        P[:, i] /= (1852 * 60) * (1852 * 60) * np.cos(LAT[i] * np.pi / 180)
+    P /= (1852 * 60) * (1852 * 60) * np.cos(LAT.reshape(-1, 1) * np.pi / 180)
 
     return P
